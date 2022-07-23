1f8792592301d4d15e93c2b5e31181381dea4b41
skyGwork <skyGworkk@gmail.com>
2022-07-23 08:38:22 +0530

    filter path setup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// basic setup
+// > basic setup
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,8 +14,13 @@ const adminRoute = require('./router/admin');
 const shopRoute = require('./router/shop');
 
 //initiate imports
-app.use(adminRoute);
+app.use('/admin', adminRoute);
 app.use(shopRoute);
+
+// 404 PAGE NOT FOUND
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 //! LISTEN APP
 app.listen(PORT, hostName, () => {
